5f6cce0a9c9b06b649f3f400a391f5f75d682a7b
Sydney Hunter <sydney.jane.hunter@gmail.com>
2020-01-05 21:41:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
-  attr_accessor :mood
-  attr_reader :name, :owner
+  attr_accessor :mood, :owner
+  attr_reader :name
 
-  def initialize(name, mood)
+  @@all = []
+
+  def initialize(name, owner)
     @name = name
     @mood = "nervous"
+    @owner = owner
+    @@all << self
   end
-end
+
+  def self.all
+    @@all
+  end
+end 
